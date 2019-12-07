cc8233712230c7638046147e43f33b28feb2ff95
KIB <kibwilap@gmail.com>
2019-12-07 20:31:47 +0800

    update App

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 // App.js
 
 import React from 'react'
-import Navigation from './Navigation/Navigation.js'
+import Search from './Components/Search.js'
 
 export default class App extends React.Component {
   render() {
     return (
-      <Navigation/>
+      <Search/>
     )
   }
 }
