ee91c0f88d2bd26bc32e488123eea272f58acfa8
KIB <kibwilap@gmail.com>
2019-12-13 16:58:38 +0800

    petite modification sur App.js et Navigation.js

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 // App.js
 
 import React from 'react'
-import Search from './Components/Search'
+import Search from './Navigation/Navigation'
 
 export default class App extends React.Component {
   render() {
     return (
-      <Search/>
+      <Navigation/>
     )
   }
 }
--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,15 +1,15 @@
 // Navigation/Navigation.js
 
-import { createStackNavigator, createAppContainer } from 'react-navigation-stack'
+import { CreateStackNavigator, CreateAppContainer } from 'react-navigation-stack'
 import Search from '../Components/Search'
 
 const SearchStackNavigator = createStackNavigator({
   Search: {
     screen: Search,
     navigationOptions: {
-      title: 'Rechercher'
+      title: 'Search'
     }
   }
 })
 
-export default createAppContainer(SearchStackNavigator)
+export default CreateAppContainer(SearchStackNavigator)
