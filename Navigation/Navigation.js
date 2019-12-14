6f28591c0ca363df547558df73c72ac6a2811fd6
KIB <kibwilap@gmail.com>
2019-12-14 14:26:25 +0800

    Update App.js & Navigation/Navigation.js

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,7 +1,11 @@
 // Navigation/Navigation.js
 
-import { createStackNavigator, createAppContainer } from 'react-navigation-stack'
+//import { createStackNavigator, createAppContainer } from 'react-navigation-stack'
+
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import Search from '../Components/Search'
+import FilmDetail from '../Components/FilmDetail'
 
 const SearchStackNavigator = createStackNavigator({
   Search: {
@@ -9,6 +13,9 @@ const SearchStackNavigator = createStackNavigator({
     navigationOptions: {
       title: 'Rechercher'
     }
+  },
+  FilmDetail: { // Encore une fois j'ai mis le même nom que celui du component mais libre à vous de choisir un nom différent
+    screen: FilmDetail
   }
 })
 
