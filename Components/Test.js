0911a37338515fd537801c3a9db6e4bd62d4ea61
KIB <kibwilap@gmail.com>
2019-12-20 18:27:26 +0800

    testing commit 97 state

--- Components/Test.js
+++ Components/Test.js
@@ -1,38 +1,14 @@
 // Components/Test.js
 
 import React from 'react'
-import { StyleSheet, View, Animated, Easing, PanResponder, Dimensions } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 
 class Test extends React.Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      topPosition: 0,
-      leftPosition: 0,
-    }
-
-    var {height, width} = Dimensions.get('window');
-    this.panResponder = PanResponder.create({
-        onStartShouldSetPanResponder: (evt, gestureState) => true,
-        onPanResponderMove: (evt, gestureState) => {
-            let touches = evt.nativeEvent.touches;
-            if (touches.length == 1) {
-                this.setState({
-                  topPosition: touches[0].pageY - height/2,
-                  leftPosition: touches[0].pageX - width/2
-                })
-            }
-        }
-    })
-  }
 
   render() {
     return (
       <View style={styles.main_container}>
-        <View
-          {...this.panResponder.panHandlers}
-          style={[styles.animation_view, { top: this.state.topPosition, left: this.state.leftPosition }]}>
+        <View style={styles.subview_container}>
         </View>
       </View>
     )
@@ -45,10 +21,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  animation_view: {
-    backgroundColor: 'red',
-    width: 100,
-    height: 100
+  subview_container: {
+
   }
 })
 
